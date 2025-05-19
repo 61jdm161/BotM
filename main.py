4abc3f9244c906eaee32336823bfce2b629d6396
61jdm161 <61jdm161@gmail.com>
2025-05-19 19:10:08 +0400

    BotM 1.0

---
+++ main.py
@@ -0,0 +1,36 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import random
+
+COMPLIMENTS = [
+    "Ты сегодня особенно прекрасна 💖",
+    "Твоя улыбка способна растопить лёд 🧊😊",
+    "Ты как солнечный луч в пасмурный день ☀️",
+    "Какая красивая сосочка ❤️",
+    "Вот это попка оууу май ❤️",
+    "Ты вдохновляешь меня каждый день ✨"
+]
+
+GIRLFRIEND_CHAT_ID = False
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я бот. Напиши пожалуйста команду /id в чат со мной")
+
+app = ApplicationBuilder().token("7853026188:AAFfYhkceinXhFDfAxoGd9t_TdZRkvxkHw0").build()
+
+async def compliment(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    compliment_text = random.choice(COMPLIMENTS)
+    try:
+        await context.bot.send_message(chat_id=GIRLFRIEND_CHAT_ID, text=compliment_text)
+        await update.message.reply_text("Комплимент отправлен!")
+    except Exception as e:
+        await update.message.reply_text(f"Ошибка: {e}")
+
+
+async def debug(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"Твой chat_id: {update.message.chat_id}")
+
+app.add_handler(CommandHandler("id", debug))
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("compliment", compliment))
+app.run_polling()
